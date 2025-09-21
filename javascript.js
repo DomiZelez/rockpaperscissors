33798db41db94dc9ce98b8ff42e29522c791eb4f
domizelez <domi.fon.zeleznik@gmail.com>
2025-09-21 13:02:39 +0200

    Added score tracking + five rounds functionality

--- javascript.js
+++ javascript.js
@@ -1,4 +1,8 @@
-let userInput = prompt("Choose rock, paper, or scissors.")
+let humanScore = 0
+let computerScore = 0
+
+for (let i = 0; i < 5; i++) {
+let userInput = prompt("Choose rock, paper, or scissors.").toLowerCase();
 
 function getHumanChoice(string) {
     if (string == "rock") {
@@ -24,4 +28,53 @@ function getComputerChoice(number) {
        return "scissors";
     }
 }
+
 console.log(getComputerChoice(choice))
+
+let humanChoice = getHumanChoice(userInput);
+let computerChoice = getComputerChoice(choice);
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        computerScore += 1;
+        return "You lose!";
+    } else if (humanChoice == "rock" && computerChoice == "rock") {
+        return "It's a tie!";
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore += 1;
+        return "You win!";
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore += 1;
+        return "You lose!";
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        return "It's a tie!";
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore += 1;
+        return "You win!";
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore += 1;
+        return "You lose!";
+    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        return "It's a tie!";
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore += 1;
+        return "You win!";
+    }
+    }
+
+console.log(playRound(humanChoice, computerChoice))
+
+console.log(humanScore + " : " + computerScore)
+}
+
+function gameResults (humanScore, computerScore) {
+    if (humanScore > computerScore) {
+        return "WINNER!";
+    } else if (humanScore < computerScore) {
+        return "GAME OVER! YOU LOST!"
+    } else {
+        return "GAME OVER! IT'S A TIE!"
+    }
+}
+
+console.log(gameResults(humanScore, computerScore))
